faf37878352c28159398396150dc19443365593e
CesarAGL <xcesaraugustoz@gmail.com>
2026-07-29 17:26:15 -0300

    Add files via upload

---
+++ actionUsuario.php
@@ -0,0 +1,173 @@
+<?php include "header.php" ?>
+
+    <?php
+        //Verifica se o método de envio das informações do form é "POST"
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            //Cria variáveis para armazenar as informações recebidas do array $_POST
+            $fotoUsuario = $nomeUsuario = $dataNascimentoUsuario = $cidadeUsuario = $emailUsuario = $senhaUsuario = $confirmarSenhaUsuario = "";
+
+            //Variável booleana para controle de erros de preenchimento
+            $erroPreenchimento = false;
+
+            //Validação do campo nomeUsuario
+            //Utiliza a função empty() para verificar se o campo está vazio
+            if(empty($_POST["nomeUsuario"])){
+                echo "<div class='alert alert-warning text-center'>O campo <strong>NOME</strong> é obrigatório!</div>";
+                $erroPreenchimento = true;
+            }
+            else{
+                //Se o $_POST["nomeUsuario"] não estiver vazio, é filtrado e armazenado na variável PHP
+                $nomeUsuario = filtrar_entrada($_POST["nomeUsuario"]);
+
+                //Utiliza a função preg_match() para verificar se há apenas letras no nome
+                if(!preg_match('/^[\p{L} ]+$/u', $nomeUsuario)){
+                    echo "<div class='alert alert-warning text-center'>O campo <strong>NOME</strong> deve conter apenas letras!</div>";
+                    $erroPreenchimento = true;
+                }
+            }
+
+            //Validação do campo dataNascimentoUsuario
+            //Utiliza a função empty() para verificar se o campo está vazio
+            if(empty($_POST["dataNascimentoUsuario"])){
+                echo "<div class='alert alert-warning text-center'>O campo <strong>DATA DE NASCIMENTO</strong> é obrigatório!</div>";
+                $erroPreenchimento = true;
+            }
+            else{
+                //Se o $_POST["dataNascimentoUsuario"] não estiver vazio, é filtrado e armazenado na variável PHP
+                $dataNascimentoUsuario = filtrar_entrada($_POST["dataNascimentoUsuario"]);
+
+                //Utiliza a função strlen() para verificar o comprimento da string $dataNascimentoUsuario (string length)
+                if(strlen($dataNascimentoUsuario) == 10){
+                    //Aplicar a função substr() para gerar substrings e armazenar dia, mês e ano de nascimento
+                    $diaNascimentoUsuario = substr($dataNascimentoUsuario, 8, 2);
+                    $mesNascimentoUsuario = substr($dataNascimentoUsuario, 5, 2);
+                    $anoNascimentoUsuario = substr($dataNascimentoUsuario, 0, 4);
+                }
+                else{
+                    echo "<div class='alert alert-warning text-center'><strong>DATA</strong> inválida!</div>";
+                    $erroPreenchimento = true;
+                }
+            }
+
+                    //Validação do campo telefoneUsuario
+        //Utiliza a função empty() para verificar se o campo está vazio
+        if (empty($_POST["telefoneUsuario"])) {
+            echo "<div class='alert alert-warning text-center'>O campo <strong>TELEFONE</strong> é obrigatório!</div>";
+            $erroPreenchimento = true;
+        } else {
+            //Se o $_POST["telefoneUsuario"] não estiver vazio, é filtrado e armazenado na variável PHP
+            $telefoneUsuario = filtrar_entrada($_POST["telefoneUsuario"]);
+
+            //Verifica se contém exatamente 11 dígitos
+            if (!preg_match('/^\d{11}$/', $telefoneUsuario)) {
+                echo "<div class='alert alert-warning text-center'>O campo <strong>TELEFONE</strong> deve conter exatamente 11 números (DDD + telefone).</div>";
+                $erroPreenchimento = true;
+            }
+        }
+
+                      //Validação do campo emailUsuario
+            //Utiliza a função empty() para verificar se o campo está vazio
+            if(empty($_POST["emailUsuario"])){
+                echo "<div class='alert alert-warning text-center'>O campo <strong>EMAIL</strong> é obrigatório!</div>";
+                $erroPreenchimento = true;
+            }
+            else{
+                //Se o $_POST["emailUsuario"] não estiver vazio, é filtrado e armazenado na variável PHP
+                $emailUsuario = filtrar_entrada($_POST["emailUsuario"]);
+            }
+
+            //Validação do campo senhaUsuario
+            //Utiliza a função empty() para verificar se o campo está vazio
+            if(empty($_POST["senhaUsuario"])){
+                echo "<div class='alert alert-warning text-center'>O campo <strong>SENHA</strong> é obrigatório!</div>";
+                $erroPreenchimento = true;
+            }
+            else{
+                //Se o $_POST["senhaUsuario"] não estiver vazio, é filtrado e armazenado na variável PHP
+                //Usa a função md5() para criptografar a senha do usuário
+                $senhaUsuario = md5(filtrar_entrada($_POST["senhaUsuario"]));
+            }
+
+            //Validação do campo confirmarSenhaUsuario
+            //Utiliza a função empty() para verificar se o campo está vazio
+            if(empty($_POST["confirmarSenhaUsuario"])){
+                echo "<div class='alert alert-warning text-center'>O campo <strong>CONFIRMAR SENHA</strong> é obrigatório!</div>";
+                $erroPreenchimento = true;
+            }
+            else{
+                //Se o $_POST["confirmarSenhaUsuario"] não estiver vazio, é filtrado e armazenado na variável PHP
+                $confirmarSenhaUsuario = md5(filtrar_entrada($_POST["confirmarSenhaUsuario"]));
+
+                //Compara se as senhas são diferentes
+                if($senhaUsuario != $confirmarSenhaUsuario){
+                    echo "<div class='alert alert-warning text-center'>As <strong>SENHAS</strong> informadas não são iguais!</div>";
+                    $erroPreenchimento = true;
+                }
+            }
+
+             //Verifica se não há erro de preenchimento
+            if(!$erroPreenchimento){
+
+                //Cria uma variável para armazenar a QUERY que realiza a inserção de dados na tabela Usuarios
+                $inserirUsuario = "INSERT INTO usuarios (nomeUsuario, dataNascimentoUsuario, telefoneUsuario, emailUsuario, senhaUsuario) VALUES ('$nomeUsuario', '$dataNascimentoUsuario', '$telefoneUsuario', '$emailUsuario', '$senhaUsuario')";
+
+                //Inclui o arquivo de conexão com o Banco de Dados
+                include "conexaoBD.php";
+
+                
+                //Usa a função mysqli_query() para executar a QUERY no Banco de Dados
+                //Se conseguir, exibe alerta de sucesso e tabela com os dados informados
+                if(mysqli_query($conn, $inserirUsuario)){
+
+                    echo "<div class='alert alert-success text-center'>O cadastro do <strong>USUÁRIO</strong> foi efetuado com sucesso!</div>";
+                    echo "
+                            <table class='table'>
+                                <tr>
+                                    <th>NOME</th>
+                                    <td>$nomeUsuario</td>
+                                </tr>
+                                <tr>
+                                    <th>DATA DE NASCIMENTO</th>
+                                    <td>$diaNascimentoUsuario/$mesNascimentoUsuario/$anoNascimentoUsuario</td>
+                                </tr>
+                                <tr>
+                                    <th>TELEFONE</th>
+                                    <td>$telefoneUsuario</td>
+                                </tr>
+                                <tr>
+                                    <th>EMAIL</th>
+                                    <td>$emailUsuario</td>
+                                </tr>
+                                <tr>
+                                    <th>SENHA</th>
+                                    <td>$senhaUsuario</td>
+                                </tr>
+                                <tr>
+                                    <th>CONFIRMAR SENHA</th>
+                                    <td>$confirmarSenhaUsuario</td>
+                                </tr>
+                            </table>
+                        </div>
+                    ";
+                }
+                else{
+                    echo "<div class='alert alert-danger text-center'>Erro ao tentar cadastrar <strong>USUÁRIO</strong> no banco de dados!</div>";
+                }
+            }
+
+        }
+        else{
+            //Usa a função header() para redirecionar o usuário para o formUsuario.php
+            header("location:formUsuario.php");
+        }
+
+        //Função para filtrar entrada de dados e evitar SQL Injection
+        function filtrar_entrada($dado){
+            $dado = trim($dado); //Remove espaços desnecessários
+            $dado = stripslashes($dado); //Remove barras invertidas
+            $dado = htmlspecialchars($dado); //Converte caracteres especiais em entidades HTML
+
+            //Após o dado passar pelos filtros, é retornado
+            return($dado);
+        }
+    ?>
---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>ReservAÍ</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/reservai.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Simple line icons-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.5.5/css/simple-line-icons.min.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <a class="menu-toggle rounded" href="#"><i class="fas fa-bars"></i></a>
+        <nav id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand"><a href="index.php">ReservAÍ</a></li>
+                <li class="sidebar-nav-item"><a href="#">Minhas reservas</a></li>
+                <li class="sidebar-nav-item"><a href="#about">Quadras</a></li>
+                <li class="sidebar-nav-item"><a href="#services">Serviços</a></li>
+                <li class="sidebar-nav-item"><a href="#portfolio">Sobre</a></li>
+                <li class="sidebar-nav-item"><a href="#contact">Contato</a></li>
+            </ul>
+        </nav>
+        <!-- Header-->
+        <!-- Cabeçalho -->
+        <header class="bg-dark py-2">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <img src="assets/img/reservai.png" style="width: 150px" class="pb-2">
+                    <p class="lead fw-normal text-white mb-0 audiowide-regular">Reservas de quadras faclitadas para você!</p>
+                </div>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="content-section bg-light" id="about">
+            <div class="container px-4 px-lg-5 text-center">
